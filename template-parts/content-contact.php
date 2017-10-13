3804088f5a8a2a6256974cb0136cfb3f0fbc039b
Fritz <fre.healy@gmail.com>
2017-10-13 10:22:32 -0400

    adding contact template and a styling

---
+++ template-parts/content-contact.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-contact"); ?>>
+    <?php $row_1_image = get_field("row_1_image");?>
+    <div class="row-1" <?php if($row_1_image):
+        echo 'style="background-image: url('.$row_1_image['url'].');"';
+    endif;?>>
+    </div><!--.row-1-->
+    <div class="row-2">
+		<div class="wrapper cap">     
+            <header class="row-1"><h1><?php the_title();?></h1></header>
+            <div class="row-2 clear-bottom">
+                <section class="col-1">
+                    <div class="copy">
+                        <?php the_content();?>
+                    </div><!--.copy-->
+                </section><!--.col-1-->
+                <aside class="col-2">
+                    <?php $map = get_field("map");
+                    if($map):
+                        echo $map;
+                    endif;?>
+                </aside><!--.col-2-->
+            </div><!--.row-2-->
+        </div><!--.wrapper-->
+    </div><!--.row-2-->
+</article><!-- #post-## -->
