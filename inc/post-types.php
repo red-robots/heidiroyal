7fd9559bfc993dd461dc0c88a9693d53f9ecd2bf
Lisa <annalisa.quiamco@gmail.com>
2019-07-24 21:28:02 -0600

    Updated About Us page and added Lawyers post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -1,45 +1,119 @@
 <?php 
 /* Custom Post Types */
+add_action('init', 'js_custom_init', 1);
+function js_custom_init() {
+  $post_types = array(
+        array(
+            'post_type' => 'slides', // Homepage Slides
+            'menu_name' => 'Slides',
+            'plural'    => 'Slides',
+            'single'    => 'Slide',
+            'supports'  => array('title','editor','thumbnail')
+        ),
+        array(
+            'post_type' => 'lawyers', // Laywers
+            'menu_name' => 'Lawyers',
+            'plural'    => 'Lawyers',
+            'single'    => 'Lawyer',
+            'supports'  => array('title','editor')
+        ),
+    );
+    
+    if($post_types) {
+        foreach ($post_types as $p) {
+            $p_type = ( isset($p['post_type']) && $p['post_type'] ) ? $p['post_type'] : ""; 
+            $single_name = ( isset($p['single']) && $p['single'] ) ? $p['single'] : "Custom Post"; 
+            $plural_name = ( isset($p['plural']) && $p['plural'] ) ? $p['plural'] : "Custom Post"; 
+            $menu_name = ( isset($p['menu_name']) && $p['menu_name'] ) ? $p['menu_name'] : $p['plural']; 
+            $menu_icon = ( isset($p['menu_icon']) && $p['menu_icon'] ) ? $p['menu_icon'] : "dashicons-admin-post"; 
+            $supports = ( isset($p['supports']) && $p['supports'] ) ? $p['supports'] : array('title','editor','custom-fields','thumbnail'); 
+            $taxonomies = ( isset($p['taxonomies']) && $p['taxonomies'] ) ? $p['taxonomies'] : array(); 
+            $parent_item_colon = ( isset($p['parent_item_colon']) && $p['parent_item_colon'] ) ? $p['parent_item_colon'] : ""; 
+            $menu_position = ( isset($p['menu_position']) && $p['menu_position'] ) ? $p['menu_position'] : 20; 
+            
+            if($p_type) {
+                
+                $labels = array(
+                    'name' => _x($plural_name, 'post type general name'),
+                    'singular_name' => _x($single_name, 'post type singular name'),
+                    'add_new' => _x('Add New', $single_name),
+                    'add_new_item' => __('Add New ' . $single_name),
+                    'edit_item' => __('Edit ' . $single_name),
+                    'new_item' => __('New ' . $single_name),
+                    'view_item' => __('View ' . $single_name),
+                    'search_items' => __('Search ' . $plural_name),
+                    'not_found' =>  __('No ' . $plural_name . ' found'),
+                    'not_found_in_trash' => __('No ' . $plural_name . ' found in Trash'), 
+                    'parent_item_colon' => $parent_item_colon,
+                    'menu_name' => $menu_name
+                );
+            
+            
+                $args = array(
+                    'labels' => $labels,
+                    'public' => true,
+                    'publicly_queryable' => true,
+                    'show_ui' => true, 
+                    'show_in_menu' => true, 
+                    'show_in_rest' => true,
+                    'query_var' => true,
+                    'rewrite' => true,
+                    'capability_type' => 'post',
+                    'has_archive' => false, 
+                    'hierarchical' => false, 
+                    'menu_position' => $menu_position,
+                    'menu_icon'=> $menu_icon,
+                    'supports' => $supports
+                ); 
+                
+                register_post_type($p_type,$args); // name used in query
+                
+            }
+            
+        }
+    }
+}
 
-add_action('init', 'js_custom_init');
-function js_custom_init() 
-{
-	
-	// Register the Homepage Slides
-  
-     $labels = array(
-	'name' => _x('Slides', 'post type general name'),
-    'singular_name' => _x('Slide', 'post type singular name'),
-    'add_new' => _x('Add New', 'Slide'),
-    'add_new_item' => __('Add New Slide'),
-    'edit_item' => __('Edit Slides'),
-    'new_item' => __('New Slide'),
-    'view_item' => __('View Slides'),
-    'search_items' => __('Search Slides'),
-    'not_found' =>  __('No Slides found'),
-    'not_found_in_trash' => __('No Slides found in Trash'), 
-    'parent_item_colon' => '',
-    'menu_name' => 'Slides'
-  );
-  $args = array(
-	'labels' => $labels,
-    'public' => true,
-    'publicly_queryable' => true,
-    'show_ui' => true, 
-    'show_in_menu' => true, 
-    'query_var' => true,
-    'rewrite' => true,
-    'capability_type' => 'post',
-    'has_archive' => false, 
-    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
-    'menu_position' => 20,
-    'supports' => array('title','editor','custom-fields','thumbnail'),
-	
-  ); 
-  register_post_type('slides',$args); // name used in query
-  
-  // Add more between here
-  
-  // and here
-  
-  } // close custom post type
+
+add_filter( 'manage_posts_columns', 'set_custom_cpt_columns' );
+function set_custom_cpt_columns($columns) {
+    global $wp_query;
+    $query = isset($wp_query->query) ? $wp_query->query : '';
+    $post_type = ( isset($query['post_type']) ) ? $query['post_type'] : '';
+    
+    if($post_type=='lawyers') {
+        unset( $columns['title'] );
+        unset( $columns['date'] );
+        $columns['title'] = __( 'Name', 'acstarter' );
+        $columns['photo'] = __( 'Photo', 'acstarter' );
+        $columns['date'] = __( 'Date', 'acstarter' );
+    }
+    
+    return $columns;
+}
+
+// Add the data to the custom columns
+add_action( 'manage_posts_custom_column' , 'custom_post_column', 10, 2 );
+function custom_post_column( $column, $post_id ) {
+    global $wp_query;
+    $query = isset($wp_query->query) ? $wp_query->query : '';
+    $post_type = ( isset($query['post_type']) ) ? $query['post_type'] : '';
+
+    if($post_type=='lawyers') {
+        switch ( $column ) {
+            case 'photo' :
+                $img = get_field('image',$post_id);
+                $img_src = ($img) ? $img['sizes']['thumbnail'] : '';
+                $the_photo = '<span class="tmphoto" style="display:inline-block;width:50px;height:50px;background:#e2e1e1;text-align:center;">';
+                if($img_src) {
+                   $the_photo .= '<img src="'.$img_src.'" alt="" style="width:100%;height:auto" />';
+                } else {
+                    $the_photo .= '<i class="dashicons dashicons-businessman" style="font-size:33px;position:relative;top:8px;left:-6px;opacity:0.3;"></i>';
+                }
+                $the_photo .= '</span>';
+                echo $the_photo;
+                break;
+        }
+    }
+}
+
--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -10,59 +10,90 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class("template-about"); ?>>
-    <?php $row_1_image = get_field("row_1_image");
-    $row_1_image_mobile = get_field("row_1_image_mobile");?>
-    <div class="row-1 <?php if($row_1_image_mobile) echo "mobile ";?>">
-        <?php if($row_1_image):?>
-            <img class="desktop" src="<?php echo $row_1_image['url'];?>" alt="<?php echo $row_1_image['alt'];?>">
-        <?php endif;?>
-        <?php if($row_1_image_mobile):?>
-            <img class="mobile" src="<?php echo $row_1_image_mobile['url'];?>" alt="<?php echo $row_1_image_mobile['alt'];?>">
-        <?php endif;?>
-    </div><!--.row-1-->
-    <div class="row-2">
-		<div class="wrapper cap">   
-            <header class="row-1"><h1><?php the_title();?></h1></header>
-            <div class="row-2 clear-bottom">
-                <aside class="col-1 copy">
-                    <?php $name = get_field("name");
-                    $linkedin = get_field("linkedin");
-                    $email = get_field("email");
-                    if($name):?>
-                        <header><h2><?php echo $name;?></h2></header>
-                        <div class="spacer"></div><!--.spacer-->
-                    <?php endif;
-                    if($linkedin ||$email):?>
-                        <div class="social">
-                            <?php if($linkedin):?>
-                                <div class="linkedin">
-                                    <a href="<?php echo $linkedin;?>">
-                                        <i class="fa fa-linkedin"></i>
-                                    </a>
-                                </div><!--linkedin-->
-                            <?php endif;
-                            if($email):?>
-                                <div class="email">
-                                    <a href="mailto:<?php echo $email;?>">
-                                        <i class="fa fa-envelope"></i>
-                                    </a>
-                                </div><!--linkedin-->
+
+   
+        <?php $row_1_image = get_field("row_1_image");
+        $row_1_image_mobile = get_field("row_1_image_mobile");?>
+        <div class="row-1 <?php if($row_1_image_mobile) echo "mobile ";?>">
+            <?php if($row_1_image):?>
+                <img class="desktop" src="<?php echo $row_1_image['url'];?>" alt="<?php echo $row_1_image['alt'];?>">
+            <?php endif;?>
+            <?php if($row_1_image_mobile):?>
+                <img class="mobile" src="<?php echo $row_1_image_mobile['url'];?>" alt="<?php echo $row_1_image_mobile['alt'];?>">
+            <?php endif;?>
+        </div><!--.row-1-->
+
+ 
+        <div class="row-2">
+            <div class="wrapper cap pagetitlediv">
+                <header class="row-1"><h1><?php echo get_the_title(); ?></h1></header>
+                <?php if (get_the_content()) { ?>
+                <div class="entry-content"><?php the_content(); ?></div> 
+                <?php } ?>
+            </div>
+            <?php  
+            $args = array(
+                'posts_per_page'   => -1,
+                'post_type'        => 'lawyers',
+                'post_status'      => 'publish'
+            );
+            $items = new WP_Query($args);
+            if ( $items->have_posts() ) { ?>
+                <?php $i=1; while ( $items->have_posts() ) : $items->the_post(); ?>
+                <div class="wrapper cap block-info block<?php echo $i?>">
+                    <div class="row-2 clear-bottom">
+                        <aside class="col-1 copy">
+                            <?php $name = get_the_title();
+                            $linkedin = get_field("linkedin");
+                            $email = get_field("email"); ?>
+                            <header><h2><?php echo $name;?></h2></header>
+                            <div class="spacer"></div>
+                            <?php if($linkedin || $email) { ?>
+                                <div class="social">
+                                    <?php if($linkedin):?>
+                                        <div class="linkedin">
+                                            <a href="<?php echo $linkedin;?>">
+                                                <i class="fa fa-linkedin"></i>
+                                            </a>
+                                        </div><!--linkedin-->
+                                    <?php endif;
+                                    if($email):?>
+                                        <div class="email">
+                                            <a href="mailto:<?php echo $email;?>">
+                                                <i class="fa fa-envelope"></i>
+                                            </a>
+                                        </div><!--linkedin-->
+                                    <?php endif;?>
+                                </div><!--.social-->
+                            <?php } else { ?>
+                            <div class="nosocial"></div>
+                            <?php } ?>
+
+                            <?php $sidebar = get_field("sidebar");
+                            if($sidebar):?>
+                                <div class="list">
+                                    <?php echo $sidebar;?>
+                                </div><!--.list-->
                             <?php endif;?>
-                        </div><!--.social-->
-                    <?php endif;?>
-                    <?php $sidebar = get_field("sidebar");
-                    if($sidebar):?>
-                        <div class="list">
-                            <?php echo $sidebar;?>
-                        </div><!--.list-->
-                    <?php endif;?>
-                </aside><!--.col-1-->
-                <section class="col-2">
-                    <div class="copy">
-                        <?php the_content();?>
-                    </div><!--.copy-->
-                </section><!--.col-2-->
-            </div><!--.row-2-->
-        </div><!--.wrapper-->
-    </div><!--.row-2-->
+                        </aside><!--.col-1-->
+
+                        <?php $pic = get_field('image'); ?>
+                        <section class="col-2">
+                            <div class="copy">
+                                <?php if ($pic) { ?>
+                                 <div class="profilepic"><img src="<?php echo $pic['url']; ?>" alt="<?php echo $pic['title']; ?>" /></div>   
+                                <?php } ?>
+                                <?php the_content();?>
+                            </div><!--.copy-->
+                        </section><!--.col-2-->
+
+                    </div>
+                </div>
+                <?php $i++; endwhile; wp_reset_postdata(); ?>
+            <?php } ?>
+
+        </div><!--.row-2-->
+     
+
+
 </article><!-- #post-## -->
